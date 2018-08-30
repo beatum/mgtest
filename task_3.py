bb1df30de2e2f6d02cf173310acbe61dedcc9ff5
I.S <direct@beatum-group.ru>
2018-08-30 15:04:24 +0300

    add

---
+++ task_3.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+"""
+Created on 29.08.2018
+@author: Ivan Semernyakov <beatuminflow@gmail.com>
+"""
+
+# ------------------------------------------------------------------------------
+# Функия words() принимает аргумент n (по умолчанию равен 0), выводит слово
+# товар в нужном склонении: 12 товаров, 22 товара, 1 товар, 0 товаров.
+# Аргумент n принимает любое целочисленное число!
+# ------------------------------------------------------------------------------
+
+
+def words(n=0):
+    if n == 0:
+        print('{} товарoв'.format(n))
+    elif n == 1:
+        print('{} товар'.format(n))
+    elif 1 < n < 5:
+        print('{} товарa'.format(n))
+    elif 4 < n < 21:
+        print('{} товаров'.format(n))
+    elif n >= 21 and n % 10 == 1:
+        print('{} товар'.format(n))
+    elif n > 21 and n % 10 == 0:
+        print('{} товаров'.format(n))
+    elif n > 21 and 1 < n % 10 < 5:
+        print('{} товара'.format(n))
+    else:
+        if n > 21 and 4 < n % 10 < 10:
+            print('{} товаров'.format(n))
+        else:
+            print('TypeError')
+
+print('-- Проверка в цикле --')
+for i in range(158):
+    print(words(i))
+
+
---
+++ task_5.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+"""
+Created on 29.08.2018
+@author: Ivan Semernyakov <beatuminflow@gmail.com>
+"""
+
+# ------------------------------------------------------------------------------
+# Фунции сортировки людей по возрасту (sortByAge) и имени (sortByName),
+# сортируют массив по именам (в алфавитном порядке) и возрасту (по убыванию).
+# Вызываем только sortByAge.
+# ------------------------------------------------------------------------------
+
+data = {
+    'Алексей': 20,
+    'Пётр': 23,
+    'Александр': 24,
+    'Михаил': 25,
+    'Фёдор': 27
+}
+
+
+def sortByAge(data):
+    if data:
+        age = list(data.values())
+        age.sort(reverse=True)
+        print('sortByAge', age)
+    else:
+        print('Sort data by name error')
+
+sortByAge(data)
+
+
+def sortByName(data):
+    if data:
+        name = list(data.keys())
+        name.sort()
+        print('sortByName', name)
+    else:
+        print('Sort data by name error')
+
+
+# sortByName(data)
